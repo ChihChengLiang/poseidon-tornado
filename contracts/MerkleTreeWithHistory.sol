9e16b07101670198c40341fc0e46789c07b5cc4b
ChihChengLiang <chihchengliang@gmail.com>
2021-08-12 13:23:59 +0800

    add contracts

---
+++ contracts/MerkleTreeWithHistory.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.7.3;
+
+interface Hasher {
+    function poseidon(bytes32[2] calldata leftRight)
+        external
+        pure
+        returns (bytes32);
+}
+
+contract MerkleTreeWithHistory {
+    uint256 public constant FIELD_SIZE =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    uint256 public constant ZERO_VALUE =
+        21663839004416932945382355908790599225266501822907911457504978515578255421292; // = keccak256("tornado") % FIELD_SIZE
+
+    Hasher public hasher;
+
+    uint32 public immutable levels;
+
+    // the following variables are made public for easier testing and debugging and
+    // are not supposed to be accessed in regular code
+    bytes32[] public filledSubtrees;
+    bytes32[] public zeros;
+    uint32 public currentRootIndex = 0;
+    uint32 public nextIndex = 0;
+    uint32 public constant ROOT_HISTORY_SIZE = 100;
+    bytes32[ROOT_HISTORY_SIZE] public roots;
+
+    constructor(uint32 _treeLevels, address _hasher) {
+        require(_treeLevels > 0, "_treeLevels should be greater than zero");
+        require(_treeLevels < 32, "_treeLevels should be less than 32");
+
+        hasher = Hasher(_hasher);
+        levels = _treeLevels;
+
+        bytes32 currentZero = bytes32(ZERO_VALUE);
+        zeros.push(currentZero);
+        filledSubtrees.push(currentZero);
+
+        for (uint32 i = 1; i < _treeLevels; i++) {
+            currentZero = hashLeftRight(currentZero, currentZero);
+            zeros.push(currentZero);
+            filledSubtrees.push(currentZero);
+        }
+
+        roots[0] = hashLeftRight(currentZero, currentZero);
+    }
+
+    /**
+    @dev Hash 2 tree leaves, returns MiMC(_left, _right)
+  */
+    function hashLeftRight(bytes32 _left, bytes32 _right)
+        public
+        view
+        returns (bytes32)
+    {
+        require(
+            uint256(_left) < FIELD_SIZE,
+            "_left should be inside the field"
+        );
+        require(
+            uint256(_right) < FIELD_SIZE,
+            "_right should be inside the field"
+        );
+        bytes32[2] memory leftright = [_left, _right];
+        return hasher.poseidon(leftright);
+    }
+
+    function _insert(bytes32 _leaf) internal returns (uint32 index) {
+        uint32 currentIndex = nextIndex;
+        require(
+            currentIndex != uint32(2)**levels,
+            "Merkle tree is full. No more leafs can be added"
+        );
+        nextIndex += 1;
+        bytes32 currentLevelHash = _leaf;
+        bytes32 left;
+        bytes32 right;
+
+        for (uint32 i = 0; i < levels; i++) {
+            if (currentIndex % 2 == 0) {
+                left = currentLevelHash;
+                right = zeros[i];
+
+                filledSubtrees[i] = currentLevelHash;
+            } else {
+                left = filledSubtrees[i];
+                right = currentLevelHash;
+            }
+
+            currentLevelHash = hashLeftRight(left, right);
+
+            currentIndex /= 2;
+        }
+
+        currentRootIndex = (currentRootIndex + 1) % ROOT_HISTORY_SIZE;
+        roots[currentRootIndex] = currentLevelHash;
+        return nextIndex - 1;
+    }
+
+    /**
+    @dev Whether the root is present in the root history
+  */
+    function isKnownRoot(bytes32 _root) public view returns (bool) {
+        if (_root == 0) return false;
+
+        uint32 i = currentRootIndex;
+        do {
+            if (_root == roots[i]) return true;
+            if (i == 0) i = ROOT_HISTORY_SIZE;
+            i--;
+        } while (i != currentRootIndex);
+        return false;
+    }
+
+    /**
+    @dev Returns the last root
+  */
+    function getLastRoot() public view returns (bytes32) {
+        return roots[currentRootIndex];
+    }
+}
---
+++ contracts/Tornado.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.7.3;
+
+import "./MerkleTreeWithHistory.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+interface IVerifier {
+    function verifyProof(bytes calldata _proof, uint256[6] calldata _input)
+        external
+        returns (bool);
+}
+
+abstract contract Tornado is MerkleTreeWithHistory, ReentrancyGuard {
+    uint256 public immutable denomination;
+    IVerifier public immutable verifier;
+
+    mapping(bytes32 => bool) public nullifierHashes;
+    // we store all commitments just to prevent accidental deposits with the same commitment
+    mapping(bytes32 => bool) public commitments;
+
+    event Deposit(
+        bytes32 indexed commitment,
+        uint32 leafIndex,
+        uint256 timestamp
+    );
+    event Withdrawal(
+        address to,
+        bytes32 nullifierHash,
+        address indexed relayer,
+        uint256 fee
+    );
+
+    /**
+    @param _verifier the address of SNARK verifier for this contract
+    @param _denomination transfer amount for each deposit
+    @param _merkleTreeHeight the height of deposits' Merkle Tree
+    */
+    constructor(
+        IVerifier _verifier,
+        uint256 _denomination,
+        uint32 _merkleTreeHeight,
+        address _hasher
+    ) MerkleTreeWithHistory(_merkleTreeHeight, _hasher) {
+        require(_denomination > 0, "denomination should be greater than 0");
+        verifier = _verifier;
+        denomination = _denomination;
+    }
+
+    /**
+    @dev Deposit funds into the contract. The caller must send (for ETH) or approve (for ERC20) value equal to or `denomination` of this instance.
+    @param _commitment the note commitment, which is PedersenHash(nullifier + secret)
+  */
+    function deposit(bytes32 _commitment) external payable nonReentrant {
+        require(!commitments[_commitment], "The commitment has been submitted");
+
+        uint32 insertedIndex = _insert(_commitment);
+        commitments[_commitment] = true;
+        _processDeposit();
+
+        emit Deposit(_commitment, insertedIndex, block.timestamp);
+    }
+
+    /** @dev this function is defined in a child contract */
+    function _processDeposit() internal virtual;
+
+    /**
+    @dev Withdraw a deposit from the contract. `proof` is a zkSNARK proof data, and input is an array of circuit public inputs
+    `input` array consists of:
+      - merkle root of all deposits in the contract
+      - hash of unique deposit nullifier to prevent double spends
+      - the recipient of funds
+      - optional fee that goes to the transaction sender (usually a relay)
+    */
+    function withdraw(
+        bytes calldata _proof,
+        bytes32 _root,
+        bytes32 _nullifierHash,
+        address payable _recipient,
+        address payable _relayer,
+        uint256 _fee,
+        uint256 _refund
+    ) external payable nonReentrant {
+        require(_fee <= denomination, "Fee exceeds transfer value");
+        require(
+            !nullifierHashes[_nullifierHash],
+            "The note has been already spent"
+        );
+        require(isKnownRoot(_root), "Cannot find your merkle root"); // Make sure to use a recent one
+        require(
+            verifier.verifyProof(
+                _proof,
+                [
+                    uint256(_root),
+                    uint256(_nullifierHash),
+                    uint256(_recipient),
+                    uint256(_relayer),
+                    _fee,
+                    _refund
+                ]
+            ),
+            "Invalid withdraw proof"
+        );
+
+        nullifierHashes[_nullifierHash] = true;
+        _processWithdraw(_recipient, _relayer, _fee, _refund);
+        emit Withdrawal(_recipient, _nullifierHash, _relayer, _fee);
+    }
+
+    /** @dev this function is defined in a child contract */
+    function _processWithdraw(
+        address payable _recipient,
+        address payable _relayer,
+        uint256 _fee,
+        uint256 _refund
+    ) internal virtual;
+
+    function isSpent(bytes32 _nullifierHash) public view returns (bool) {
+        return nullifierHashes[_nullifierHash];
+    }
+
+    function isSpentArray(bytes32[] calldata _nullifierHashes)
+        external
+        view
+        returns (bool[] memory spent)
+    {
+        spent = new bool[](_nullifierHashes.length);
+        for (uint256 i = 0; i < _nullifierHashes.length; i++) {
+            if (isSpent(_nullifierHashes[i])) {
+                spent[i] = true;
+            }
+        }
+    }
+}
