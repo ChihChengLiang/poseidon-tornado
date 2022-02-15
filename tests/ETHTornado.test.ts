fa8ebf2f028b4f8f30bba2975d2a8af734073a1c
oskarth <ot@oskarthoren.com>
2022-02-15 11:42:12 +0800

    Update tests/ETHTornado.test.ts
    Co-authored-by: Chih Cheng Liang <chihchengliang@gmail.com>

--- tests/ETHTornado.test.ts
+++ tests/ETHTornado.test.ts
@@ -18,10 +18,12 @@ const HEIGHT = 20;
 
 function poseidonHash(poseidon: any, inputs: BigNumberish[]): string {
     const hash = poseidon(inputs.map((x) => BigNumber.from(x).toBigInt()));
+    // Make the number within the field size
+    const hashStr = poseidon.F.toString(hash);
+    // Make it a valid hex string
+    const hashHex = BigNumber.from(hashStr).toHexString();
     // pad zero to make it 32 bytes, so that the output can be taken as a bytes32 contract argument
-    const bytes32 = ethers.utils.hexZeroPad(
-        BigNumber.from(hash).toHexString(),
-        32
+    const bytes32 = ethers.utils.hexZeroPad(hashHex, 32);
     );
     return bytes32;
 }
